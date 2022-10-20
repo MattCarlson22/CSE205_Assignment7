eaef38a94c202be5f8877c87c47ff19498c94c44
MattCarlson22 <MattCarlson22@protonmail.com>
2022-10-20 12:31:54 -0700

    Initial commit

---
+++ src/CompanyAddressComparator.java
@@ -0,0 +1,18 @@
+//Assignment #: ASU Fall 2022 #7
+//Name:
+//StudentID:
+//Lecture:
+//Description:
+
+import java.util.Comparator;
+
+public class CompanyAddressComparator implements Comparator<Company>
+{
+	//First we compare the state info., if they are same, we then compare their city info.
+	//If the two company addresses have the same state AND city info. then we should compare
+	//their street info. The companies should be listed in the alphabetical order of their addresses!
+	public int compare(Company first, Company second)
+	{
+		//----
+	}
+}
---
+++ src/CompanyManagement.java
@@ -0,0 +1,53 @@
+//Assignment #: ASU Fall 2022 #7
+//Name:
+//StudentID:
+//Lecture:
+//Description: //---- is where you should add your own code
+
+//import all necessary classes and interfaces
+//----
+
+public class CompanyManagement
+{
+	ArrayList<Company> companyList;
+
+	public CompanyManagement() {
+		//----
+	}
+
+	public int companyExists(String companyName, String companyAddress) {
+		//----
+	}
+
+	public int addressExists(String street, String city, String state) {
+		//----
+	}
+
+	public boolean addCompany(String companyName, int numOfEmployees, String nameOfCEO, String street, String city, String state) {
+		//----
+	}
+
+	public boolean removeCompany(String companyName, String companyAddress) {
+		//----
+	}
+
+	public void sortByCompanyName() {
+		//----
+	}
+
+	public void sortByEmployeeNumbers() {
+		//----
+	}
+
+	public void sortByCompanyAddress() {
+		//----
+	}
+
+	public String listCompanys() {
+		//----
+	}
+
+	public void closeCompanyManagement() {
+		//----
+	}
+}
